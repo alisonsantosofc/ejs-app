6cb7db3f4cd370c8cab7e4bbd05b916be1d857b3
Alison Santos <alisonsantoso22@gmail.com>
2022-02-05 11:16:42 -0300

    Project finished

--- server.js
+++ server.js
@@ -1,17 +1,51 @@
 const express = require('express');
+const favicon = require('serve-favicon');
+const path = require('path');
 
 const app = express();
+
+const employes = [
+  {
+    name: 'Roberto Fontes',
+    team: 'Front-end',
+    admission_at: '28/04/2021',
+  },
+  {
+    name: 'Adriana Rabelo',
+    team: 'Designer UI/UX',
+    admission_at: '28/04/2021',
+  },
+  {
+    name: 'Jefersson Silva',
+    team: 'Front-end',
+    admission_at: '28/04/2021',
+  },
+  {
+    name: 'Raquel Torres',
+    team: 'Back-end',
+    admission_at: '28/04/2021',
+  },
+  {
+    name: 'Fernando Leal',
+    team: 'Back-end',
+    admission_at: '28/04/2021',
+  },
+];
 
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-  res.render('index');
+  res.render('pages/index', {
+    data: employes,
+  });
 });
 
 app.get('/about', function (req, res) {
-  res.render('about');
+  res.render('pages/about');
 });
 
+app.use(favicon(path.join(__dirname, 'views', 'favicon.ico')));
+
 app.listen(7000, () => {
-  console.log('✅️ Server started successfuly!');
+  console.log('✅️ Server started successfuly on http://localhost:7000');
 });
